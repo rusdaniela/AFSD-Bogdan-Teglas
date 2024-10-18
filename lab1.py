855bba6eb62172fafdbbfd86f6c1261b3a9acfb4
Bogdan <bogdanioanteglas@yahoo.com>
2024-10-18 11:56:07 +0300

    Tema 2

--- lab1.py
+++ lab1.py
@@ -1,2 +1,1 @@
-print("Bogdan Teglas")
 print("hi")
---
+++ lab2.py
@@ -0,0 +1,24 @@
+varsta = 20					# int
+medie = (10+5+8)/3			# float
+nume = "Cosmin Sabo"		# sting
+
+# print(varsta)
+# print(medie)
+# print(nume)
+
+# INDEXARE  []
+# print(nume)
+# print(nume[len(nume)-1])
+
+# CARACTERE SPECIALE
+
+# \n = newline
+
+# \t = tab
+
+# operatori pe string-uri
+# + concatenare
+# print("Salut" + " " + nume)
+
+# * multiplicare
+print(nume*30)
