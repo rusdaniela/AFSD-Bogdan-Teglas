a5c4da98905658b71561e32023bbfdd8ad526a95
alexandruont <alex.ont@icloud.com>
2025-10-07 09:46:04 +0300

    print in main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 if __name__ == '__main__':
     print("Hello, World!?!")
     print("Hello, World!?!")
-    print("hi, git!")
-
-
-print("Hello HElloHello HElloHello HElloHello HElloHello HElloHello HElloHello HEllo")
+    print("hi, git!")   
+    print("Hello HElloHello HElloHello HElloHello HElloHello HElloHello HElloHello HEllo")
