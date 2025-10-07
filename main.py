fa41798d5ed0ed236aa10d3c51996d1b57801080
PMiro06 <popmiroslav1999@gmail.com>
2025-10-07 09:47:55 +0300

    .

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     print("Hello, World!?!")
     print("Hello, World!?!")
     print("hi, git!")
-    print("Te pup Miro")
+    print("Miro")
     print("hi, git!")
     print("Hello HElloHello HElloHello HElloHello HElloHello HElloHello HElloHello HEllo")
